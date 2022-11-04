8c4528ec19babbc3f0039f3270e1f0f4a1bc2774
Oleksandr Andriienko <oandriie@redhat.com>
2022-11-04 02:10:08 +0200

    Temp.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- aws/main.go
+++ aws/main.go
@@ -32,7 +32,7 @@ func main() {
 			HostnameImmutable: true,
 		}, nil
 	})
-	credentialsOpt := config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider("console", "console123", ""))
+	credentialsOpt := config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider("storage-user", "console123", ""))
 
 	cfg, err := config.LoadDefaultConfig(context.TODO(),
 		config.WithEndpointResolverWithOptions(customResolver),
